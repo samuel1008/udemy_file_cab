195949da9c01c9f6dc34bac335a91e484c8fea85
Sam <samuel1008@gmail.com>
2016-03-22 02:49:45 -0700

    created index list

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,15 +1,27 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
+    @docs = Doc.all
   end
 
   def show
+
   end
 
   def new
+    @doc = Doc.new
   end
 
   def create
+    @doc = Doc.new(doc_params)
+    #if the doc or doc.save, we want the doc to redirect us to the doc itself, else we want to render a new file
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
+
   end
 
   def edit
@@ -20,5 +32,15 @@ class DocsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def find_doc
+      @doc = Doc.find(params[:id])
+    end
+
+    def doc_params
+      params.require(:doc).permit(:title, :content)
+    end
 
 end
