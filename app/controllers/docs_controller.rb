d5346f1532826452800be6b50ec3b7ab5e69a877
Sam <samuel1008@gmail.com>
2016-03-22 01:32:52 -0700

    added a model and view for docs

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,24 @@
+class DocsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def upated
+  end
+
+  def destroy
+  end
+
+end
